1c4a32461abca30ead84cc8a0d7a6aa152dfa3b2
Djalma Araujo <djalma@close.io>
2018-01-12 20:43:50 +0000

    Add multiple life cycle methods. Super useful

--- index.js
+++ index.js
@@ -19,7 +19,14 @@ export default class PhotoUpload extends React.Component {
     width: PropTypes.number,
     format: PropTypes.string,
     quality: PropTypes.number,
-    onPhotoSelect: PropTypes.func // returns the base64 string of uploaded photo
+    onPhotoSelect: PropTypes.func, // returns the base64 string of uploaded photo
+    onError: PropTypes.func, // if any error occur with response
+    onTapCustomButton: PropTypes.func, // on tap custom button
+    onStart: PropTypes.func, // when user starts (useful for loading, etc)
+    onCancel: PropTypes.func, // when user cancel
+    onResponse: PropTypes.func, // on response exists!
+    onRender: PropTypes.func, // after render
+    onResizedImageUri: PropTypes.func, // when image resized is ready
   }
 
   state = {
@@ -38,18 +45,25 @@ export default class PhotoUpload extends React.Component {
   }
 
   openImagePicker = () => {
+    if (this.props.onStart) this.props.onStart()
+
     // get image from image picker
     ImagePicker.showImagePicker(this.options, async response => {
       console.log('Response = ', response)
 
+      if (this.props.onResponse) this.props.onResponse(response)
+
       if (response.didCancel) {
         console.log('User cancelled image picker')
+        this.props.onCancel('User cancelled image picker')
         return
       } else if (response.error) {
         console.log('ImagePicker Error: ', response.error)
+        this.props.onError(response.error)
         return
       } else if (response.customButton) {
         console.log('User tapped custom button: ', response.customButton)
+        this.props.onTapCustomButton(response.customButton)
         return
       }
 
@@ -63,6 +77,9 @@ export default class PhotoUpload extends React.Component {
         format,
         quality
       )
+
+      if (this.props.onResizedImageUri) this.props.onResizedImageUri(resizedImageUri)
+
       const filePath = Platform.OS === 'android' && resizedImageUri.uri.replace
         ? resizedImageUri.uri.replace('file:/data', '/data')
         : resizedImageUri.uri
@@ -75,9 +92,7 @@ export default class PhotoUpload extends React.Component {
       })
 
       // handle photo in props functions as data string
-      if (this.props.onPhotoSelect) {
-        this.props.onPhotoSelect(photoData)
-      }
+      if (this.props.onPhotoSelect) this.props.onPhotoSelect(photoData)
     })
   }
 
@@ -91,7 +106,11 @@ export default class PhotoUpload extends React.Component {
     })
   }
 
-  render () {
+  componentDidUpdate() {
+    if (this.props.onAfterRender) this.props.onAfterRender(this.state)
+  }
+
+  render() {
     return (
       <View style={[styles.container, this.props.containerStyle]}>
         <TouchableOpacity onPress={this.openImagePicker}>
